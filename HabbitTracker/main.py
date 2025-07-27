8326c2e2d9747808e49b9d4c218f9faf43d2e660
AdwaitDeshmukh <66303441+AdwaitDeshmukh@users.noreply.github.com>
2025-07-27 23:55:26 +0530

    creating graph on pixela

--- HabbitTracker/main.py
+++ HabbitTracker/main.py
@@ -2,11 +2,27 @@ import requests
 
 
 pixela_endpoint="https://pixe.la/v1/users"
+username="adwaitdeshmukh"
+token="adwaitnitinpallavi"
 user_params={
-    "token":"sampletoken",
+    "token":"adwaitnitinpallavi",
     "username":"adwaitdeshmukh",
     "agreeTermsOfService":"yes",
     "notMinor":"yes"
 }
-response=requests.post(url=pixela_endpoint,json=user_params)
-print(response.json())
+# response=requests.post(url=pixela_endpoint,json=user_params)
+# print(response.json())
+
+graph_endpoint=f"{pixela_endpoint}/{username}/graphs"
+graph_config={
+    "id":"graph1",
+    "name":"coding graph",
+    "unit":"programs ",
+    "type":"int",
+    "color":"sora"
+}
+headers={
+    "X-USER-TOKEN":token
+}
+response_graph=requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+print(response_graph.text)
