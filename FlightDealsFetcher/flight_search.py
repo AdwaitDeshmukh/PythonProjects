ecb9367aff5b1381ff6943b9963be385dd22d981
AdwaitDeshmukh <66303441+AdwaitDeshmukh@users.noreply.github.com>
2025-07-31 23:24:23 +0530

    Create flight_search.py

---
+++ FlightDealsFetcher/flight_search.py
@@ -0,0 +1,7 @@
+API_KEY="confidential"
+API_SECRET="confidential"
+class FlightSearch:
+    def get_iata_code(self,city_name):
+        return "testing"
+    #This class is responsible for talking to the Flight Search API.
+    pass
